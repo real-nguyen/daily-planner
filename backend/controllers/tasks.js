6190a4c0e03a69c9b01087092cef506dfda8c9f6
Real Nguyen <real.nguyen@hotmail.com>
2020-06-23 12:32:30 -0400

    Further optimized fetchTasks -> reduce nesting

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,33 +1,48 @@
 const Task = require('../models/task');
-const TaskType = require('../models/task-type');
 
 exports.fetchTasks = (req, res, next) => {
-  TaskType.find().or([{ type: 'Priority' }, { type: 'Recurrent' }]).then(types => {
-    const priority = types[0];
-    const recurrent = types[1];
-    // Get priority and recurrent tasks
-    // sort by hours required, descending
-    // group by task type
-    Task.aggregate()
-    .match({ $or: [{type: priority._id}, {type: recurrent._id}] })
-    .sort({ hoursRequired: -1 })
-    .group({
-      _id: '$type',
-      tasks: {
-        $push: {
-          _id: '$_id',
-          task: '$task',
-          type: '$type',
-          done: '$done',
-          hoursRequired: '$hoursRequired'
+  // Join with TaskType schema on Type.type == TaskType._id
+  // Filter out free time tasks
+  // Get priority and recurrent tasks
+  // Sort by hours required, descending
+  // Group by task type
+  Task.aggregate()
+  .lookup({
+    from: 'tasktypes',
+      let: { type_id: '$type' },
+      pipeline: [{
+        $match: {
+          $expr: {
+            $and: [
+              { $eq: ['$$type_id', '$_id'] },
+              { $not: { $eq: ['$type', 'Free Time'] } }
+            ]
+          }
         }
-      }
-    })
-    .then(result => {
-      console.log(result);
-      res.status(200).json({
-        tasks: result
-      });
-    });
+      }],
+      as: 'type'
   })
+  // Type.type is returned as an array after lookup; unpack it
+  .unwind({ path: '$type' })
+  .sort({ hoursRequired: -1 })
+  .group({
+    _id: '$type.type',
+    tasks: {
+      $push: {
+        _id: '$_id',
+        task: '$task',
+        type: '$type',
+        done: '$done',
+        hoursRequired: '$hoursRequired'
+      }
+    }
+  })
+  .then(result => {
+    const priorityTasks = result.find(t => t._id == 'Priority');
+    const recurrentTasks = result.find(t => t._id == 'Recurrent');
+    res.status(200).json({
+      priorityTasks: priorityTasks,
+      recurrentTasks, recurrentTasks
+    });
+  });
 };
